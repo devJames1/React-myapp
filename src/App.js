74e460922a3d1a229fbf2a19082da2e45c771094
James Okolie <zebedee788@gmail.com>
2023-06-06 15:40:21 +0600

    On branch master Changes to be committed: 	modified:   src/App.js 	new file:   src/Components/ClassComp.js 	new file:   src/Components/Click.js 	new file:   src/Components/FunctionalComp.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import FC from './Components/FunctionalComp';
+import { ClassComp, ClassComp1 } from './Components/ClassComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Welcome to Simplilearn</h1>
+      <h1>This videoisabout Components</h1>
+      <FC />
+      <ClassComp />
+      <ClassComp1 />
     </div>
   );
 }
